8c1ccddc2fdbed018f92f3303d3f9ae37823723e
USUARIO <USUARIO@DINALAN>
2025-02-15 14:22:33 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DinApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            Variable = new ListBox();
+            Opc = new TabControl();
+            Opciones = new TabPage();
+            lblServer = new Label();
+            txtServer = new TextBox();
+            lblDatabase = new Label();
+            txtDatabase = new TextBox();
+            lblUserId = new Label();
+            txtUserId = new TextBox();
+            lblPassword = new Label();
+            txtPassword = new TextBox();
+            lblStatus = new Label();
+            Datos = new TabPage();
+            loadTablesButton = new Button();
+            queryButton = new Button();
+            txtQuery = new TextBox();
+            Opc.SuspendLayout();
+            Opciones.SuspendLayout();
+            Datos.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(6, 193);
+            button1.Name = "button1";
+            button1.Size = new Size(302, 73);
+            button1.TabIndex = 0;
+            button1.Text = "Abrir BD";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnConnect_Click;
+            // 
+            // Variable
+            // 
+            Variable.Cursor = Cursors.No;
+            Variable.FormattingEnabled = true;
+            Variable.ItemHeight = 15;
+            Variable.Location = new Point(314, 7);
+            Variable.Name = "Variable";
+            Variable.Size = new Size(179, 259);
+            Variable.TabIndex = 1;
+            // 
+            // Opc
+            // 
+            Opc.Controls.Add(Opciones);
+            Opc.Controls.Add(Datos);
+            Opc.Cursor = Cursors.Hand;
+            Opc.Location = new Point(12, 12);
+            Opc.Name = "Opc";
+            Opc.SelectedIndex = 0;
+            Opc.Size = new Size(507, 301);
+            Opc.TabIndex = 2;
+            Opc.Tag = "Hola";
+            // 
+            // Opciones
+            // 
+            Opciones.Controls.Add(Variable);
+            Opciones.Controls.Add(button1);
+            Opciones.Controls.Add(lblServer);
+            Opciones.Controls.Add(txtServer);
+            Opciones.Controls.Add(lblDatabase);
+            Opciones.Controls.Add(txtDatabase);
+            Opciones.Controls.Add(lblUserId);
+            Opciones.Controls.Add(txtUserId);
+            Opciones.Controls.Add(lblPassword);
+            Opciones.Controls.Add(txtPassword);
+            Opciones.Controls.Add(lblStatus);
+            Opciones.Location = new Point(4, 24);
+            Opciones.Name = "Opciones";
+            Opciones.Padding = new Padding(3);
+            Opciones.Size = new Size(499, 273);
+            Opciones.TabIndex = 0;
+            Opciones.Text = "Opciones";
+            Opciones.UseVisualStyleBackColor = true;
+            // 
+            // lblServer
+            // 
+            lblServer.AutoSize = true;
+            lblServer.Location = new Point(6, 10);
+            lblServer.Name = "lblServer";
+            lblServer.Size = new Size(53, 15);
+            lblServer.TabIndex = 3;
+            lblServer.Text = "Servidor:";
+            // 
+            // txtServer
+            // 
+            txtServer.Location = new Point(92, 7);
+            txtServer.Name = "txtServer";
+            txtServer.Size = new Size(216, 23);
+            txtServer.TabIndex = 4;
+            // 
+            // lblDatabase
+            // 
+            lblDatabase.AutoSize = true;
+            lblDatabase.Location = new Point(6, 50);
+            lblDatabase.Name = "lblDatabase";
+            lblDatabase.Size = new Size(82, 15);
+            lblDatabase.TabIndex = 5;
+            lblDatabase.Text = "Base de datos:";
+            // 
+            // txtDatabase
+            // 
+            txtDatabase.Location = new Point(92, 47);
+            txtDatabase.Name = "txtDatabase";
+            txtDatabase.Size = new Size(216, 23);
+            txtDatabase.TabIndex = 6;
+            // 
+            // lblUserId
+            // 
+            lblUserId.AutoSize = true;
+            lblUserId.Location = new Point(6, 90);
+            lblUserId.Name = "lblUserId";
+            lblUserId.Size = new Size(50, 15);
+            lblUserId.TabIndex = 7;
+            lblUserId.Text = "Usuario:";
+            // 
+            // txtUserId
+            // 
+            txtUserId.Location = new Point(92, 87);
+            txtUserId.Name = "txtUserId";
+            txtUserId.Size = new Size(216, 23);
+            txtUserId.TabIndex = 8;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(6, 130);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(70, 15);
+            lblPassword.TabIndex = 9;
+            lblPassword.Text = "Contraseña:";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(92, 127);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(216, 23);
+            txtPassword.TabIndex = 10;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.ForeColor = Color.Red;
+            lblStatus.Location = new Point(6, 166);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(123, 15);
+            lblStatus.TabIndex = 11;
+            lblStatus.Text = "Estado: Desconectado";
+            // 
+            // Datos
+            // 
+            Datos.Controls.Add(loadTablesButton);
+            Datos.Controls.Add(queryButton);
+            Datos.Controls.Add(txtQuery);
+            Datos.Location = new Point(4, 24);
+            Datos.Name = "Datos";
+            Datos.Padding = new Padding(3);
+            Datos.Size = new Size(499, 273);
+            Datos.TabIndex = 1;
+            Datos.Text = "Datos";
+            Datos.UseVisualStyleBackColor = true;
+            // 
+            // loadTablesButton
+            // 
+            loadTablesButton.Location = new Point(81, 3);
+            loadTablesButton.Name = "loadTablesButton";
+            loadTablesButton.Size = new Size(75, 23);
+            loadTablesButton.TabIndex = 2;
+            loadTablesButton.Text = "See All Tables";
+            loadTablesButton.UseVisualStyleBackColor = true;
+            loadTablesButton.Click += LoadQuery;
+            // 
+            // queryButton
+            // 
+            queryButton.Location = new Point(2, 3);
+            queryButton.Name = "queryButton";
+            queryButton.Size = new Size(75, 23);
+            queryButton.TabIndex = 1;
+            queryButton.Text = "New Query";
+            queryButton.UseVisualStyleBackColor = true;
+            queryButton.Click += NewQuery;
+            // 
+            // txtQuery
+            // 
+            txtQuery.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtQuery.Location = new Point(0, 27);
+            txtQuery.Multiline = true;
+            txtQuery.Name = "txtQuery";
+            txtQuery.Size = new Size(500, 1140);
+            txtQuery.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(531, 325);
+            Controls.Add(Opc);
+            Name = "Form1";
+            Text = "Formulario";
+            Opc.ResumeLayout(false);
+            Opciones.ResumeLayout(false);
+            Opciones.PerformLayout();
+            Datos.ResumeLayout(false);
+            Datos.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Variable;
+        private System.Windows.Forms.TabControl Opc;
+        private System.Windows.Forms.TabPage Opciones;
+        private System.Windows.Forms.TabPage Datos;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label lblUserId;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button queryButton;
+        private System.Windows.Forms.Button loadTablesButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+using System;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Evento del botón para conectar
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Recoger los datos ingresados por el usuario en los TextBox
+            string server = txtServer.Text;  // txtServer es el TextBox para el servidor
+            string database = txtDatabase.Text;  // txtDatabase es el TextBox para la base de datos
+            string userId = txtUserId.Text;  // txtUserId es el TextBox para el usuario
+            string password = txtPassword.Text;  // txtPassword es el TextBox para la contraseña
+
+            // Crear la cadena de conexión
+            string connectionString = $"Server={server}; Database={database}; User Id={userId}; Password={password};";
+
+            // Intentar conectar con la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();  // Establecer la conexión
+                    lblStatus.Text = "Conexión exitosa!";
+                    lblStatus.ForeColor = System.Drawing.Color.Green;  // Cambiar color de texto a verde
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al obtener las bases de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    lblStatus.ForeColor = System.Drawing.Color.Red;  // Cambiar color de texto a rojo
+                }
+            }
+        }
+    
+        private void NewQuery(object sender, EventArgs e)
+        {
+            String newquery = txtQuery.Text;
+        }
+
+        private void LoadQuery(object sender, EventArgs e)
+        {
+            txtQuery.Text = "SELECT table_name\r\nFROM information_schema.tables\r\nWHERE table_type = 'BASE TABLE'";
+        }
+    }
+}
