cb6b9465b5042bbde2e6ffa50a591b8e973b0b93
Alain Fernandez <alainfervi@gmail.com>
2025-02-17 23:49:24 +0100

    Ssistema implementado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,6 +3,25 @@
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabConnection;
+        private System.Windows.Forms.TabPage tabQuery;
+        private System.Windows.Forms.TabPage tabSignalComparison;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnExecuteQuery;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnCompareSignals;
+        private System.Windows.Forms.TextBox txtSignals;
 
         protected override void Dispose(bool disposing)
         {
@@ -15,177 +34,229 @@
 
         private void InitializeComponent()
         {
-            button1 = new Button();
-            Opc = new TabControl();
-            Opciones = new TabPage();
-            lblServer = new Label();
+            tabControl = new TabControl();
+            tabConnection = new TabPage();
+            btnConnect = new Button();
             txtServer = new TextBox();
-            lblDatabase = new Label();
+            lblServer = new Label();
             txtDatabase = new TextBox();
+            lblDatabase = new Label();
+            txtUser = new TextBox();
+            lblUser = new Label();
+            txtPassword = new TextBox();
+            lblPassword = new Label();
             lblStatus = new Label();
-            Datos = new TabPage();
-            loadTablesButton = new Button();
-            queryButton = new Button();
+            tabQuery = new TabPage();
+            btnExecuteQuery = new Button();
             txtQuery = new TextBox();
-            Opc.SuspendLayout();
-            Opciones.SuspendLayout();
-            Datos.SuspendLayout();
+            lstResults = new ListBox();
+            tabSignalComparison = new TabPage();
+            btnCompareSignals = new Button();
+            txtSignals = new TextBox();
+            tabControl.SuspendLayout();
+            tabConnection.SuspendLayout();
+            tabQuery.SuspendLayout();
+            tabSignalComparison.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // tabControl
             // 
-            button1.Cursor = Cursors.Hand;
-            button1.ForeColor = Color.Black;
-            button1.Location = new Point(6, 193);
-            button1.Name = "button1";
-            button1.Size = new Size(255, 73);
-            button1.TabIndex = 0;
-            button1.Text = "Abrir BD";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += btnConnect_Click;
+            tabControl.Controls.Add(tabConnection);
+            tabControl.Controls.Add(tabQuery);
+            tabControl.Controls.Add(tabSignalComparison);
+            tabControl.Location = new Point(12, 12);
+            tabControl.Name = "tabControl";
+            tabControl.SelectedIndex = 0;
+            tabControl.Size = new Size(350, 420);
+            tabControl.TabIndex = 0;
             // 
-            // Opc
+            // tabConnection
             // 
-            Opc.Controls.Add(Opciones);
-            Opc.Controls.Add(Datos);
-            Opc.Cursor = Cursors.Hand;
-            Opc.Location = new Point(12, 12);
-            Opc.Name = "Opc";
-            Opc.SelectedIndex = 0;
-            Opc.Size = new Size(507, 301);
-            Opc.TabIndex = 2;
-            Opc.Tag = "Hola";
+            tabConnection.Controls.Add(btnConnect);
+            tabConnection.Controls.Add(txtServer);
+            tabConnection.Controls.Add(lblServer);
+            tabConnection.Controls.Add(txtDatabase);
+            tabConnection.Controls.Add(lblDatabase);
+            tabConnection.Controls.Add(txtUser);
+            tabConnection.Controls.Add(lblUser);
+            tabConnection.Controls.Add(txtPassword);
+            tabConnection.Controls.Add(lblPassword);
+            tabConnection.Controls.Add(lblStatus);
+            tabConnection.Location = new Point(4, 24);
+            tabConnection.Name = "tabConnection";
+            tabConnection.Padding = new Padding(3);
+            tabConnection.Size = new Size(342, 392);
+            tabConnection.TabIndex = 0;
+            tabConnection.Text = "Conexión";
+            tabConnection.UseVisualStyleBackColor = true;
             // 
-            // Opciones
+            // btnConnect
             // 
-            Opciones.Controls.Add(button1);
-            Opciones.Controls.Add(lblServer);
-            Opciones.Controls.Add(txtServer);
-            Opciones.Controls.Add(lblDatabase);
-            Opciones.Controls.Add(txtDatabase);
-            Opciones.Controls.Add(lblStatus);
-            Opciones.Location = new Point(4, 24);
-            Opciones.Name = "Opciones";
-            Opciones.Padding = new Padding(3);
-            Opciones.Size = new Size(499, 273);
-            Opciones.TabIndex = 0;
-            Opciones.Text = "Opciones";
-            Opciones.UseVisualStyleBackColor = true;
+            btnConnect.Location = new Point(12, 12);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(100, 23);
+            btnConnect.TabIndex = 0;
+            btnConnect.Text = "Conectar";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // txtServer
+            // 
+            txtServer.Location = new Point(93, 60);
+            txtServer.Name = "txtServer";
+            txtServer.Size = new Size(216, 23);
+            txtServer.TabIndex = 1;
             // 
             // lblServer
             // 
             lblServer.AutoSize = true;
-            lblServer.Location = new Point(7, 103);
+            lblServer.Location = new Point(12, 63);
             lblServer.Name = "lblServer";
-            lblServer.Size = new Size(53, 15);
-            lblServer.TabIndex = 3;
-            lblServer.Text = "Servidor:";
+            lblServer.Size = new Size(74, 15);
+            lblServer.TabIndex = 2;
+            lblServer.Text = "Servidor (IP):";
             // 
-            // txtServer
+            // txtDatabase
             // 
-            txtServer.Location = new Point(93, 100);
-            txtServer.Name = "txtServer";
-            txtServer.Size = new Size(216, 23);
-            txtServer.TabIndex = 4;
+            txtDatabase.Location = new Point(93, 100);
+            txtDatabase.Name = "txtDatabase";
+            txtDatabase.Size = new Size(216, 23);
+            txtDatabase.TabIndex = 3;
             // 
             // lblDatabase
             // 
             lblDatabase.AutoSize = true;
-            lblDatabase.Location = new Point(7, 143);
+            lblDatabase.Location = new Point(12, 103);
             lblDatabase.Name = "lblDatabase";
-            lblDatabase.Size = new Size(82, 15);
-            lblDatabase.TabIndex = 5;
-            lblDatabase.Text = "Base de datos:";
+            lblDatabase.Size = new Size(83, 15);
+            lblDatabase.TabIndex = 4;
+            lblDatabase.Text = "Base de Datos:";
             // 
-            // txtDatabase
+            // txtUser
             // 
-            txtDatabase.Location = new Point(93, 140);
-            txtDatabase.Name = "txtDatabase";
-            txtDatabase.Size = new Size(216, 23);
-            txtDatabase.TabIndex = 6;
+            txtUser.Location = new Point(93, 140);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(216, 23);
+            txtUser.TabIndex = 5;
+            // 
+            // lblUser
+            // 
+            lblUser.AutoSize = true;
+            lblUser.Location = new Point(12, 143);
+            lblUser.Name = "lblUser";
+            lblUser.Size = new Size(47, 15);
+            lblUser.TabIndex = 6;
+            lblUser.Text = "Usuario";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(93, 180);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(216, 23);
+            txtPassword.TabIndex = 7;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(12, 183);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(67, 15);
+            lblPassword.TabIndex = 8;
+            lblPassword.Text = "Contraseña";
             // 
             // lblStatus
             // 
             lblStatus.AutoSize = true;
-            lblStatus.ForeColor = Color.Red;
-            lblStatus.Location = new Point(6, 166);
+            lblStatus.Location = new Point(12, 220);
             lblStatus.Name = "lblStatus";
             lblStatus.Size = new Size(123, 15);
-            lblStatus.TabIndex = 11;
+            lblStatus.TabIndex = 9;
             lblStatus.Text = "Estado: Desconectado";
             // 
-            // Datos
+            // tabQuery
             // 
-            Datos.Controls.Add(loadTablesButton);
-            Datos.Controls.Add(queryButton);
-            Datos.Controls.Add(txtQuery);
-            Datos.Location = new Point(4, 24);
-            Datos.Name = "Datos";
-            Datos.Padding = new Padding(3);
-            Datos.Size = new Size(499, 273);
-            Datos.TabIndex = 1;
-            Datos.Text = "Datos";
-            Datos.UseVisualStyleBackColor = true;
+            tabQuery.Controls.Add(btnExecuteQuery);
+            tabQuery.Controls.Add(txtQuery);
+            tabQuery.Controls.Add(lstResults);
+            tabQuery.Location = new Point(4, 24);
+            tabQuery.Name = "tabQuery";
+            tabQuery.Padding = new Padding(3);
+            tabQuery.Size = new Size(342, 392);
+            tabQuery.TabIndex = 1;
+            tabQuery.Text = "Consulta SQL";
+            tabQuery.UseVisualStyleBackColor = true;
             // 
-            // loadTablesButton
+            // btnExecuteQuery
             // 
-            loadTablesButton.Location = new Point(81, 3);
-            loadTablesButton.Name = "loadTablesButton";
-            loadTablesButton.Size = new Size(75, 23);
-            loadTablesButton.TabIndex = 2;
-            loadTablesButton.Text = "See All Tables";
-            loadTablesButton.UseVisualStyleBackColor = true;
-            loadTablesButton.Click += LoadQuery;
-            // 
-            // queryButton
-            // 
-            queryButton.Location = new Point(2, 3);
-            queryButton.Name = "queryButton";
-            queryButton.Size = new Size(75, 23);
-            queryButton.TabIndex = 1;
-            queryButton.Text = "New Query";
-            queryButton.UseVisualStyleBackColor = true;
-            queryButton.Click += NewQuery;
+            btnExecuteQuery.Location = new Point(9, 6);
+            btnExecuteQuery.Name = "btnExecuteQuery";
+            btnExecuteQuery.Size = new Size(100, 23);
+            btnExecuteQuery.TabIndex = 10;
+            btnExecuteQuery.Text = "Ejecutar Consulta";
+            btnExecuteQuery.UseVisualStyleBackColor = true;
+            btnExecuteQuery.Click += btnExecuteQuery_Click;
             // 
             // txtQuery
             // 
-            txtQuery.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            txtQuery.Location = new Point(0, 27);
+            txtQuery.Location = new Point(9, 35);
             txtQuery.Multiline = true;
             txtQuery.Name = "txtQuery";
-            txtQuery.Size = new Size(500, 1140);
-            txtQuery.TabIndex = 0;
+            txtQuery.Size = new Size(324, 52);
+            txtQuery.TabIndex = 11;
+            // 
+            // lstResults
+            // 
+            lstResults.FormattingEnabled = true;
+            lstResults.ItemHeight = 15;
+            lstResults.Location = new Point(9, 103);
+            lstResults.Name = "lstResults";
+            lstResults.Size = new Size(324, 274);
+            lstResults.TabIndex = 12;
+            // 
+            // tabSignalComparison
+            // 
+            tabSignalComparison.Controls.Add(btnCompareSignals);
+            tabSignalComparison.Controls.Add(txtSignals);
+            tabSignalComparison.Location = new Point(4, 24);
+            tabSignalComparison.Name = "tabSignalComparison";
+            tabSignalComparison.Padding = new Padding(3);
+            tabSignalComparison.Size = new Size(342, 392);
+            tabSignalComparison.TabIndex = 2;
+            tabSignalComparison.Text = "Comparar Señales";
+            tabSignalComparison.UseVisualStyleBackColor = true;
+            // 
+            // btnCompareSignals
+            // 
+            btnCompareSignals.Location = new Point(12, 12);
+            btnCompareSignals.Name = "btnCompareSignals";
+            btnCompareSignals.Size = new Size(132, 23);
+            btnCompareSignals.TabIndex = 13;
+            btnCompareSignals.Text = "Comparar Señales";
+            btnCompareSignals.UseVisualStyleBackColor = true;
+            btnCompareSignals.Click += btnCompareSignals_Click;
+            // 
+            // txtSignals
+            // 
+            txtSignals.Location = new Point(12, 41);
+            txtSignals.Multiline = true;
+            txtSignals.Name = "txtSignals";
+            txtSignals.Size = new Size(324, 345);
+            txtSignals.TabIndex = 14;
             // 
             // Form1
             // 
-            ClientSize = new Size(531, 325);
-            Controls.Add(Opc);
+            ClientSize = new Size(374, 450);
+            Controls.Add(tabControl);
             Name = "Form1";
-            Text = "Formulario";
-            Opc.ResumeLayout(false);
-            Opciones.ResumeLayout(false);
-            Opciones.PerformLayout();
-            Datos.ResumeLayout(false);
-            Datos.PerformLayout();
+            Text = "Conexión y Consultas SQL";
+            tabControl.ResumeLayout(false);
+            tabConnection.ResumeLayout(false);
+            tabConnection.PerformLayout();
+            tabQuery.ResumeLayout(false);
+            tabQuery.PerformLayout();
+            tabSignalComparison.ResumeLayout(false);
+            tabSignalComparison.PerformLayout();
             ResumeLayout(false);
         }
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox Variable;
-        private System.Windows.Forms.TabControl Opc;
-        private System.Windows.Forms.TabPage Opciones;
-        private System.Windows.Forms.TabPage Datos;
-        private System.Windows.Forms.Label lblServer;
-        private System.Windows.Forms.TextBox txtServer;
-        private System.Windows.Forms.Label lblDatabase;
-        private System.Windows.Forms.TextBox txtDatabase;
-        private System.Windows.Forms.Label lblUserId;
-        private System.Windows.Forms.TextBox txtUserId;
-        private System.Windows.Forms.Label lblPassword;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.Label lblStatus;
-        private System.Windows.Forms.TextBox txtQuery;
-        private System.Windows.Forms.Button queryButton;
-        private System.Windows.Forms.Button loadTablesButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Data;
 using Microsoft.Data.SqlClient;
 using System.Windows.Forms;
 
-//Añadir usuario Guest para correcto funcionamiento
 namespace DinApp
 {
     public partial class Form1 : Form
     {
+        SqlConnection connection = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -16,39 +18,146 @@ namespace DinApp
         private void btnConnect_Click(object sender, EventArgs e)
         {
             // Recoger los datos ingresados por el usuario en los TextBox
-            string server = txtServer.Text;  // txtServer es el TextBox para el servidor
-            string database = txtDatabase.Text;  // txtDatabase es el TextBox para la base de datos
-            string userId = txtUserId.Text;  // txtUserId es el TextBox para el usuario
-            string password = txtPassword.Text;  // txtPassword es el TextBox para la contraseña User Id={userId}; Password = {password};
+            string server = txtServer.Text.Trim();
+            string database = txtDatabase.Text.Trim();
+            string user = txtUser.Text.Trim(); // Agregar TextBox para usuario
+            string password = txtPassword.Text.Trim(); // Agregar TextBox para contraseña
 
             // Crear la cadena de conexión
-            string connectionString = $"Server={server}; Database={database};TrustServerCertificate=True;" ;
+            if (string.IsNullOrEmpty(server) || string.IsNullOrEmpty(database))
+            {
+                MessageBox.Show("Servidor y Base de Datos son obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Determinar el tipo de autenticación
+            string connectionString;
+            if (!string.IsNullOrEmpty(user) && !string.IsNullOrEmpty(password))
+            {
+                connectionString = $"Server={server}; Database={database}; User Id={user}; Password={password}; TrustServerCertificate=True;";
+            }
+            else
+            {
+                connectionString = $"Server={server}; Database={database}; TrustServerCertificate=True;";
+            }
 
             // Intentar conectar con la base de datos
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            connection = new SqlConnection(connectionString); // Mueve la creación de la conexión fuera de using
+
+            try
+            {
+                connection.Open();  // Establecer la conexión
+                lblStatus.Text = "Conexión exitosa!";
+                lblStatus.ForeColor = System.Drawing.Color.Green;  // Cambiar color de texto a verde
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener las bases de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lblStatus.ForeColor = System.Drawing.Color.Red;  // Cambiar color de texto a rojo
+            }
+        }
+
+        // Evento para ejecutar la consulta
+        private void btnExecuteQuery_Click(object sender, EventArgs e)
+        {
+            string query = txtQuery.Text.Trim(); // Obtener el texto de la consulta del TextBox
+
+            // Comprobar si la consulta está vacía
+            if (string.IsNullOrEmpty(query))
+            {
+                MessageBox.Show("Por favor, ingresa una consulta.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Ejecutar la consulta y mostrar los resultados
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
                 try
                 {
-                    connection.Open();  // Establecer la conexión
-                    lblStatus.Text = "Conexión exitosa!";
-                    lblStatus.ForeColor = System.Drawing.Color.Green;  // Cambiar color de texto a verde
+                    // Crear un SqlDataReader para leer los resultados de la consulta
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    // Limpiar el ListBox antes de mostrar nuevos resultados
+                    lstResults.Items.Clear();
+
+                    // Leer los resultados
+                    while (reader.Read())
+                    {
+                        // Para este ejemplo, asumimos que la consulta devuelve una sola columna (puedes ajustarlo según el tipo de consulta)
+                        lstResults.Items.Add(reader[0].ToString());
+                    }
+
+                    reader.Close();  // Cerrar el SqlDataReader
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Error al obtener las bases de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    lblStatus.ForeColor = System.Drawing.Color.Red;  // Cambiar color de texto a rojo
+                    MessageBox.Show("Error al ejecutar la consulta: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-        }
-    
-        private void NewQuery(object sender, EventArgs e)
-        {
-            String newquery = txtQuery.Text;
         }
 
         private void LoadQuery(object sender, EventArgs e)
         {
             txtQuery.Text = "SELECT table_name\r\nFROM information_schema.tables\r\nWHERE table_type = 'BASE TABLE'";
+        }
+
+        private void btnCompareSignals_Click(object sender, EventArgs e)
+        {
+            // Obtener las señales aportadas (pueden venir de un TextBox o de otro control)
+            string[] señalesAportadas = txtSignals.Text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Llamar a la base de datos para obtener las plantillas
+            string query = "SELECT TemplateTag, TemplateId, Mapeado FROM Plantillas";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    string mejorPlantilla = "";
+                    string mejorId = "";
+                    int maxCoincidencias = 0;
+
+                    // Leer las plantillas y comparar las señales
+                    while (reader.Read())
+                    {
+                        string templateTag = reader["TemplateTag"].ToString();
+                        string Id = reader["TemplateId"].ToString();
+                        string mapeado = reader["Mapeado"].ToString();
+
+                        // Dividir las señales mapeadas de la plantilla
+                        string[] señalesPlantilla = mapeado.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        // Contar las coincidencias
+                        int coincidencias = señalesAportadas.Count(s => señalesPlantilla.Contains(s.Trim())); //Este metodo es totalmente temporal ya que no se como se diferencia los mapeados.
+
+                        // Si encontramos más coincidencias, actualizar el mejor resultado
+                        if (coincidencias > maxCoincidencias)
+                        {
+                            maxCoincidencias = coincidencias;
+                            mejorPlantilla = templateTag;
+                            mejorId = Id;
+                        }
+                    }
+
+                    reader.Close();
+
+                    // Mostrar la plantilla más parecida
+                    if (string.IsNullOrEmpty(mejorPlantilla))
+                    {
+                        MessageBox.Show("No se encontraron coincidencias.", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"La plantilla más parecida es: {mejorPlantilla} con el ID: {mejorId}", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al ejecutar la consulta: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
